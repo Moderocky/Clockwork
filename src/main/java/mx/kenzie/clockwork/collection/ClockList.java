58140b74ef6aacba65a464bb37e41d2c1891d2bf
Moderocky <admin@moderocky.com>
2023-02-11 11:20:04 +0000

    Add clock list.

---
+++ src/main/java/mx/kenzie/clockwork/collection/ClockList.java
@@ -0,0 +1,170 @@
+package mx.kenzie.clockwork.collection;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ClockList<Type> implements List<Type> {
+
+    protected final Class<Type> type;
+    protected final List<Type> list;
+    private final Type[] array;
+
+    @SuppressWarnings("unchecked")
+    public ClockList(Class<Type> type, List<Type> list) {
+        this.type = type;
+        this.list = list;
+        this.array = (Type[]) Array.newInstance(type, 0);
+    }
+
+    @SafeVarargs
+    @SuppressWarnings("unchecked")
+    public ClockList(Type... entries) {
+        this((Class<Type>) entries.getClass().getComponentType(), new ArrayList<>(Arrays.asList(entries)));
+    }
+
+    public ClockList(Class<Type> type, int size) {
+        this(type, new ArrayList<>(size));
+    }
+
+    public ClockList(Class<Type> type) {
+        this(type, new ArrayList<>());
+    }
+
+    public Class<Type> getType() {
+        return type;
+    }
+
+    public boolean canAccept(Object object) {
+        return object == null || type.isInstance(object);
+    }
+
+    public Type getRandom() {
+        return this.getRandom(ThreadLocalRandom.current());
+    }
+
+    public Type getRandom(Random random) {
+        if (list.isEmpty()) throw new IllegalStateException("Cannot pull random element from empty list.");
+        return list.get(random.nextInt(list.size()));
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public Iterator<Type> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public Type[] toArray() {
+        return list.toArray(array);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public boolean add(Type type) {
+        return list.add(type);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return list.contains(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Type> c) {
+        return list.addAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Type> c) {
+        return list.addAll(index, c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return list.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return list.retainAll(c);
+    }
+
+    @Override
+    public void clear() {
+        this.list.clear();
+    }
+
+    @Override
+    public Type get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public Type set(int index, Type element) {
+        return list.set(index, element);
+    }
+
+    @Override
+    public void add(int index, Type element) {
+        this.list.add(index, element);
+    }
+
+    @Override
+    public Type remove(int index) {
+        return list.remove(index);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<Type> listIterator() {
+        return list.listIterator();
+    }
+
+    @Override
+    public ListIterator<Type> listIterator(int index) {
+        return list.listIterator(index);
+    }
+
+    @Override
+    public ClockList<Type> subList(int fromIndex, int toIndex) {
+        return new ClockList<>(type, list.subList(fromIndex, toIndex));
+    }
+
+    public ClockList<Type> clone() {
+        return new ClockList<>(type, new ArrayList<>(list));
+    }
+
+}
