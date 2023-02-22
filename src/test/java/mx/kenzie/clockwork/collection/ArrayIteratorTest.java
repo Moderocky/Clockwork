8306924e6975ecdcc48a48df8af291f1e30b61a8
Moderocky <admin@moderocky.com>
2023-02-22 09:20:33 +0000

    Add tests for array iterators.

---
+++ src/test/java/mx/kenzie/clockwork/collection/ArrayIteratorTest.java
@@ -0,0 +1,54 @@
+package mx.kenzie.clockwork.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+public class ArrayIteratorTest {
+
+    @Test
+    public void dynamic() {
+        final String[] strings = {"hello", "there"};
+        final Iterator<String> iterator = new ArrayIterator<>(strings);
+        assert iterator.hasNext();
+        assert iterator.next().equals("hello");
+        strings[1] = "kenobi";
+        assert iterator.next().equals("kenobi");
+        assert !iterator.hasNext();
+    }
+
+    @Test
+    public void next() {
+        final String[] strings = {"hello", "there"};
+        final Iterator<String> iterator = new ArrayIterator<>(strings);
+        assert iterator.hasNext();
+        assert iterator.next().equals("hello");
+        assert iterator.next().equals("there");
+        assert !iterator.hasNext();
+    }
+
+    @Test
+    public void remove() {
+        final String[] strings = {"hello", "there"};
+        final Iterator<String> iterator = new ArrayIterator<>(strings);
+        assert iterator.next().equals("hello");
+        iterator.remove();
+        assert iterator.next().equals("there");
+        iterator.remove();
+        assert !iterator.hasNext();
+        assert strings[0] == null;
+        assert strings[1] == null;
+    }
+
+    @Test
+    public void hasNext() {
+        final String[] strings = {"hello", "there"};
+        final Iterator<String> iterator = new ArrayIterator<>(strings);
+        assert iterator.hasNext();
+        assert iterator.next().equals("hello");
+        assert iterator.hasNext();
+        assert iterator.next().equals("there");
+        assert !iterator.hasNext();
+    }
+
+}
