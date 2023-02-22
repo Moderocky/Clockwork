90f68335f8b18bfe0193b7cbcd094d77d999983f
Moderocky <admin@moderocky.com>
2023-02-22 09:56:05 +0000

    Add removal and collection behaviour.

--- src/main/java/mx/kenzie/clockwork/collection/FixedArrayList.java
+++ src/main/java/mx/kenzie/clockwork/collection/FixedArrayList.java
@@ -1,6 +1,9 @@
 package mx.kenzie.clockwork.collection;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.AbstractList;
 import java.util.Arrays;
 import java.util.RandomAccess;
@@ -11,13 +14,29 @@ public class FixedArrayList<Type> extends AbstractList<Type> implements RandomAc
     protected int pointer;
 
     public FixedArrayList(Type[] array) {
-        this.array = array;
+        this.array = initialShuffle(array, array.length);
         this.check();
     }
 
     @SafeVarargs
     public FixedArrayList(int length, Type... elements) {
-        this(Arrays.copyOf(elements, length));
+        this.array = initialShuffle(elements, length);
+        this.check();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <Type> Type[] initialShuffle(Type[] dirty, int length) {
+        final Type[] array = (Type[]) Array.newInstance(dirty.getClass().getComponentType(), length);
+        int index = 0;
+        for (Type type : dirty) if (type != null) array[index++] = type;
+        return array;
+    }
+
+    protected void shuffle() {
+        final Type[] copy = Arrays.copyOf(array, array.length);
+        int index = 0;
+        Arrays.fill(array, null);
+        for (Type type : copy) if (type != null) array[index++] = type;
     }
 
     protected void check() {
@@ -31,13 +50,28 @@ public class FixedArrayList<Type> extends AbstractList<Type> implements RandomAc
     }
 
     @Override
+    public Type @NotNull [] toArray() {
+        return Arrays.copyOf(array, pointer);
+    }
+
+    @Override
+    public Type remove(int index) {
+        if (index >= pointer) return null;
+        final Type thing = array[index];
+        this.array[index] = null;
+        System.arraycopy(array, index + 1, array, index, array.length - (index + 1));
+        this.pointer--;
+        return thing;
+    }
+
+    @Override
     public Type get(int index) {
         return array[index];
     }
 
     @Override
     public int size() {
-        return array.length;
+        return pointer;
     }
 
     public boolean isEmpty() {
