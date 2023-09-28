9e0f628578ef9aae36f0516f69029669b667f628
Moderocky <admin@moderocky.com>
2023-09-28 19:03:05 +0100

    Make wait controllable.

--- src/main/java/mx/kenzie/clockwork/io/IOQueue.java
+++ src/main/java/mx/kenzie/clockwork/io/IOQueue.java
@@ -8,55 +8,58 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 public class IOQueue extends ConcurrentLinkedQueue<DataTask> {
-
+    
     private final ExecutorService service = Executors.newSingleThreadExecutor();
     protected volatile boolean closing;
-
+    
     public IOQueue() {
-        final Runnable runnable = () -> {
+        this(50);
+    }
+    
+    public IOQueue(int wait) {
+        this.service.submit(() -> {
             while (!closing || !this.isEmpty()) {
                 try {
                     final DataTask task = this.poll();
                     if (task == null) {
                         if (closing) break;
-                        sleep(50);
+                        sleep(wait);
                         continue;
                     }
                     task.run();
                     task.finish();
                 } catch (Throwable ex) {
                     System.err.println("Error in I/O process:");
-                    ex.printStackTrace();
+                    ex.printStackTrace(System.err);
                 }
             }
-        };
-        this.service.submit(runnable);
+        });
     }
-
+    
     protected static void sleep(long millis) {
         LockSupport.parkNanos(millis * 1000000L);
     }
-
-    public DataTask queue(DataTask task) {
+    
+    public synchronized DataTask queue(DataTask task) {
         if (!this.contains(task)) this.add(task);
         return task;
     }
-
+    
     public void shutdown(long millis) {
         final List<Runnable> tasks = service.shutdownNow();
         try {
             if (!tasks.isEmpty() && !service.awaitTermination(millis, TimeUnit.MILLISECONDS))
                 this.report(tasks);
         } catch (InterruptedException ex) {
-            ex.printStackTrace();
+            ex.printStackTrace(System.err);
             this.report(tasks);
         }
     }
-
+    
     private void report(List<Runnable> list) {
         if (list.isEmpty()) return;
         System.err.println(list.size() + " pending control tasks failed to finish.");
         for (Runnable runnable : list) System.err.println(runnable.getClass() + ": " + runnable);
     }
-
+    
 }
